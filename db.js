d0f17ff218a3fea8a32f58405a59768497fc8998
Zehan Khan <zehan9211@gmail.com>
2022-10-07 19:40:34 +0530

    all routes written and working

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: 'postgres',
-    password:process.env.PG_PASSWORD,
+    password:process.env.PASSWORD,
     host:"localhost",
     port: 5432,
     database: "todo_database"
--- index.js
+++ index.js
@@ -18,21 +18,83 @@ app.use(require("express").json());
 //* @desc      create a todo
 //* @route     POST /todos
 
-app.post( "/todos", async ( req, res ) => {
+app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
             "INSERT INTO todo (description) VALUES ($1) RETURNING *",
-            [ description ]
+            [description]
         );
-        res.json(newTodo);
-    } catch ( error ) {
+        res.json(newTodo.rows[0]);
+    } catch (error) {
         console.error(error.message)
     }
-} )
+})
+
+//* @desc      get all todos
+//* @route     GET /todos
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//* @desc      get a single todo
+//* @route     GET /todos/:todoId
+
+app.get("/todos/:todoId", async (req, res) => {
+    const { todoId } = req.params;
+    try {
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [todoId]
+        );
+        if (!todo.rows.length) {
+            res.status(401).json({ error: "todo not found" })
+        } else {
+            res.json(todo.rows);
+        }
+    } catch (error) {
+        console.error(error.message)
+        res.status(401).json({ error: "Something went wrong!!" })
+    }
+})
+
+//* @desc      update a todo
+//* @route     PUT /todos/:todoId
+
+app.put( "/todos/:todoId", async ( req, res ) => {
+    try {
+        const { todoId } = req.params;
+        const { description } = req.body;
+
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [ description, todoId ] )
+        res.json({message:"todo updated"})
+    } catch ( error ) {
+        console.error(error.message)
+        res.status(401).json({ error: "Something went wrong!!" });
+    }
+})
 
 
+//* @desc      delete a todo 
+//* @route     DELETE /todos/:todoId
 
-app.listen( port,()=>{
+app.get( "/todos/:todoId", async ( req, res ) => {
+    try {
+        const { todoId } = req.params;
+        const deletedTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [todoId]);
+        res.json({message:"todo delete successfully!!"})
+    } catch ( error ) {
+        console.error(error.message)
+        res.status(401).json({ error: "Something went wrong!!" });
+    }
+})
+
+app.listen(port, () => {
     console.log(`port survive on port ${port}`)
 })
